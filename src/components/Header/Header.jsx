4fe6635c325314ac72ff7890f444454c67682a05
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-07-02 23:14:39 +0300

    feat: add mobilr menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as Icon } from "./menuIcon.svg";
 import "./Header.css";
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState();
+
   return (
     <header className="header">
       <span className="header__logo">Logo</span>
+      <nav className={`header__nav ${isOpen ? "active" : ""}`}>
+        <ul className="header__nav-list">
+          <li className="header__nav-item">Main</li>
+          <li className="header__nav-item">About</li>
+          <li className="header__nav-item">Address</li>
+          <li className="header__nav-item">Contacts</li>
+          <li className="header__nav-item">logIn</li>
+        </ul>
+      </nav>
+      <button className="header__menu-button" onClick={() => setOpen(!isOpen)}>
+        <Icon />
+      </button>
     </header>
   );
 };
