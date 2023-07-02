39e957fbb8a88a1ab78b684878e2b27e39100f41
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-07-02 10:49:21 +0300

    feat: add base layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+  return <footer className="footer"></footer>;
+};
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Main.css";
+
+export const Main = () => {
+  return (
+    <main className="main">
+      <section className="section"></section>
+    </main>
+  );
+};
